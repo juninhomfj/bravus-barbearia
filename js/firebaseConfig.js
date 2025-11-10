cb1a43f294bc1889297ed06d3c096e5b45635ed2
juninhomfj <74722586+juninhomfj@users.noreply.github.com>
2025-11-09 23:07:47 -0300

    Criar firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Arquivo: /js/firebaseConfig.js
+
+const firebaseConfig = {
+  apiKey: window.FIREBASE_API_KEY,
+  authDomain: "bravus-barbearia.firebaseapp.com",
+  projectId: "bravus-barbearia",
+  storageBucket: "bravus-barbearia.firebasestorage.app",
+  messagingSenderId: "646053512759",
+  appId: "1:646053512759:web:e7882b0f0b2bf1d00aa1b2",
+  measurementId: "G-NH4F7JFDJW"
+};
